2307020b46453ec02e7160221841e12c22c819ce
Haryo Waskito <haryo.waskito@banksinarmas.com>
2025-09-21 18:28:50 +0700

    Fix CS8802: Convert Program.cs to use Main method

--- Program.cs
+++ Program.cs
@@ -1,34 +1,42 @@
 
 using GraniteChatbot.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace GraniteChatbot;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// Register HttpClient and our Replicate service
-builder.Services.AddHttpClient<IReplicateService, ReplicateService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        // Register HttpClient and our Replicate service
+        builder.Services.AddHttpClient<IReplicateService, ReplicateService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-app.Run();
